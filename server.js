ffa093801549c9811e600109a3ef16a9d41b7aad
abhi <abhishekchoudharyorg@gmail.com>
2026-03-01 19:33:09 +0530

    updated server to use process.env.PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./src/app')
 
 require('dotenv').config();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port dedicated')
 })
 
